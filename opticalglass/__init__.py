a11396d387e334f2d7f62088a36a1aac5386c0b7
Mike Hayford <mjhoptics@gmail.com>
2018-08-28 13:23:11 -0700

    updates for setup and additions for __init__.py

---
+++ opticalglass/__init__.py
@@ -0,0 +1,5 @@
+import opticalglass as glass
+from .glassfactory import create_glass
+from .hoya import HoyaGlass
+from .ohara import OharaGlass
+from .schott import SchottGlass
--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rayoptics",
+    name="opticalglass",
     version="0.1.5.dev1",
     author="Michael J Hayford",
     author_email="mjhoptics@gmail.com",
-    description="Tools for image forming optical design and analysis",
+    description="Tools for reading commercial optical glass catalogs",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="BSD-3-Clause",
-    url="https://github.com/mjhoptics/ray-optics",
+    url="https://github.com/mjhoptics/opticalglass",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "xlrd",
+        "numpy",
+        ],
 )
