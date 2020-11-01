eee10da579e38155af829dc7ef87a9f60de41b71
Mike Hayford <mjhoptics@gmail.com>
2020-11-01 10:58:50 -0800

    another cleanup pass, new test versio v0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2020, Michael J. Hayford'
 author = 'Michael J. Hayford'
 
 # The short X.Y version
-version = '0.6.1'
+version = '0.6.2'
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="opticalglass",
-    version="0.6.1",
+    version="0.6.2",
     author="Michael J Hayford",
     author_email="mjhoptics@gmail.com",
     description="Tools for reading commercial optical glass catalogs",
