5706cb9a7c60002b2f6f8e976d54e56d28a6fcd2
Mike Hayford <mjhoptics@gmail.com>
2021-02-01 15:13:33 -0800

    version 0.7.2

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2017-2021, Michael J. Hayford'
 author = 'Michael J. Hayford'
 
 # The short X.Y version
-version = '0.7.1'
+version = '0.7.2'
 # The full version, including alpha/beta/rc tags
-release = '0.7.1'
+release = '0.7.2'
 
 
 # -- General configuration ---------------------------------------------------
