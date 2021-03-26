a9f15053e2a4f5689141e5f4c51e48b24c719976
Mike Hayford <mjhoptics@gmail.com>
2021-03-26 10:22:58 -0700

    Modify compare_indices to use glass.meas_rindex() interface.

--- opticalglass/test/util.py
+++ opticalglass/test/util.py
@@ -21,13 +21,12 @@ def compare_indices(tc, glass, catalog, tol=5e-6, remove_lines=None):
         remove_lines: a list of strings **not** to be used in the comparisons,
                       used when data isn't available for that line
     """
-    nline = catalog.nline_str
     slines = list(spectral_lines)
     if remove_lines is not None:
         for sl in remove_lines:
             slines.remove(sl)
     for sline in slines:
         n_calc = glass.rindex(sline)
-        n_meas = glass.glass_item(nline[sline])
+        n_meas = glass.meas_rindex(sline)
         tc.assertAlmostEqual(n_calc, n_meas, delta=tol,
                              msg=f"spectral line '{sline:}' outside tol")
